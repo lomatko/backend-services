ae938c2428fea77bd18beb1f69cc24065f27bd38
Architect <berdnyk1994@gmail.com>
2023-10-20 13:16:42 +0200

    initial

---
+++ src/main/java/com/collabothon/lomatko/lomatko/LomatkoApplication.java
@@ -0,0 +1,14 @@
+package com.collabothon.lomatko.lomatko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomatkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LomatkoApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/collabothon/lomatko/lomatko/LomatkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabothon.lomatko.lomatko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LomatkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
