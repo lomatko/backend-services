eeb85d28d7e61d768a270696164a208e45ac7f92
Krzysztof Tylka <krzysztof.tylka@wp.pl>
2023-10-20 19:43:40 +0200

    Updated reward controller, repository

--- src/main/java/com/collabothon/lomatko/reward/RewardRepository.java
+++ src/main/java/com/collabothon/lomatko/reward/RewardRepository.java
@@ -1,4 +1,12 @@
 package com.collabothon.lomatko.reward;
 
-public class RewardRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RewardRepository extends JpaRepository<RewardEntity, Long> {
+
+    List<RewardEntity> findAll();
 }
