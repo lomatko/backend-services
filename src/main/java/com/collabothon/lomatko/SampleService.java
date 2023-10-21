97d428cde8cbbc6c0014e6610108007e6047a7f2
Krzysztof Tylka <krzysztof.tylka@wp.pl>
2023-10-21 13:37:01 +0200

    Updated application

--- src/main/java/com/collabothon/lomatko/SampleService.java
+++ src/main/java/com/collabothon/lomatko/SampleService.java
@@ -7,6 +7,8 @@ import com.collabothon.lomatko.customer.CustomerRepository;
 import com.collabothon.lomatko.event.EventEntity;
 import com.collabothon.lomatko.event.EventRepository;
 import com.collabothon.lomatko.event.EventStatus;
+import com.collabothon.lomatko.reward.RewardEntity;
+import com.collabothon.lomatko.reward.RewardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
@@ -20,12 +22,14 @@ import java.util.Collections;
 public class SampleService {
     private final CustomerRepository customerRepository;
     private final EventRepository eventRepository;
+    private final RewardRepository rewardRepository;
 
     @EventListener
     public void handleContextStart(ContextStartedEvent cse) {
         System.out.println("Handling context started event.");
         loadCustomer();
         loadEvent();
+        loadReward();
 
         CustomerEntity entityLoaded = customerRepository.findById(1L).orElseThrow(() -> new RuntimeException("nie ma takiego entity"));
         Customer customer = CustomerMapper.INSTANCE.mapToCustomer(entityLoaded);
@@ -55,6 +59,34 @@ public class SampleService {
                 .build();
 
         customerRepository.save(customerEntity);
+        customerRepository.flush();
+    }
+
+    private void loadReward() {
+        RewardEntity rewardEntity1 = RewardEntity.builder()
+                .id(1L)
+                .name("reward #1")
+                .description("opis 1")
+                .price(22)
+                .build();
+
+        RewardEntity rewardEntity2 = RewardEntity.builder()
+                .id(2L)
+                .name("reward #2")
+                .description("opis 2")
+                .price(5)
+                .build();
+
+        RewardEntity rewardEntity3 = RewardEntity.builder()
+                .id(3L)
+                .name("reward #3")
+                .description("opis 3")
+                .price(30)
+                .build();
+
+        rewardRepository.save(rewardEntity1);
+        rewardRepository.save(rewardEntity2);
+        rewardRepository.save(rewardEntity3);
         customerRepository.flush();
     }
 }
