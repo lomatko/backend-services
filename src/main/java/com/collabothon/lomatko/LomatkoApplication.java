9ef25c606b9c0c30c729db0cee7ede460a2d374d
Krzysztof Tylka <krzysztof.tylka@wp.pl>
2023-10-22 01:11:20 +0200

    Updated application

--- src/main/java/com/collabothon/lomatko/LomatkoApplication.java
+++ src/main/java/com/collabothon/lomatko/LomatkoApplication.java
@@ -23,7 +23,7 @@ public class LomatkoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("http://localhost:3000", "https://www.collabcoin.org")
 						.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
