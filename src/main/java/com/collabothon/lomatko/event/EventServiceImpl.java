c7ad771088788a8131c7e25f7939bb26fb5de91b
michal <michal.grebowiec@gmail.com>
2023-10-21 22:31:07 +0200

    endpoint for leaving event

--- src/main/java/com/collabothon/lomatko/event/EventServiceImpl.java
+++ src/main/java/com/collabothon/lomatko/event/EventServiceImpl.java
@@ -5,12 +5,14 @@ import com.collabothon.lomatko.customer.CustomerRepository;
 import com.collabothon.lomatko.organization.OrganizationEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -42,7 +44,6 @@ class EventServiceImpl implements EventService {
 
         List<CustomerEntity> volunteers = event.getVolunteers();
         if (volunteers == null) {
-            System.out.println("=========mamy null!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             volunteers = new ArrayList<>();
         }
         volunteers.add(customer);
@@ -53,6 +54,33 @@ class EventServiceImpl implements EventService {
             events = new ArrayList<>();
         }
         events.add(event);
+        customerRepository.saveAndFlush(customer.toBuilder().events(events).build());
+    }
+
+    @Override
+    public void leaveEvent(Long customerId, Long eventId) {
+        Objects.requireNonNull(customerId, "Can't leave event because customerId is null!");
+        Objects.requireNonNull(eventId, "Can't leave event because eventId is null!");
+
+        CustomerEntity customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer with id: " + customerId + " doesn't exist"));
+        EventEntity event = eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event with id: " + eventId + " doesn't exist"));
+
+        if (event.getVolunteers() == null) {
+            throw new IllegalStateException("Can't leave event because event doesn't contain any volunteer.");
+        }
+
+        if (customer.getEvents() == null) {
+            throw new IllegalStateException("Can't leave event because customer doesn't contain any event.");
+        }
+
+        List<CustomerEntity> volunteers = event.getVolunteers().stream()
+                .filter(volunteer -> !customerId.equals(volunteer.getId()))
+                .collect(Collectors.toList());
+        eventRepository.saveAndFlush(event.toBuilder().volunteers(volunteers).build());
+
+        List<EventEntity> events = customer.getEvents().stream()
+                .filter(customerEvent -> !eventId.equals(customerEvent.getId()))
+                .collect(Collectors.toList());
         customerRepository.saveAndFlush(customer.toBuilder().events(events).build());
     }
 }
